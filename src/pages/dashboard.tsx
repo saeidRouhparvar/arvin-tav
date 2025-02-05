24ab97482deccfd02d25559adf8aa5040d35a33a
saeid rouhparvar <saeid.rohparvar@gmail.com>
2025-02-05 23:56:16 +0330

    add dashboard components

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { Flex } from "@chakra-ui/react"
+import Topbar from "../components/topbar"
+import Statistics from "../components/statistics"
+import MainChart from "../components/mainChart"
+import OtherCharts from "../components/otherCharts"
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+   <Flex direction={'column'} gap={'1.5rem'}>
+    <Topbar/>
+    <Statistics/>
+    <MainChart/>
+    <OtherCharts/>
+   </Flex>
   )
 }
 
