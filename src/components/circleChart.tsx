f5695aa52d1d76f774cef81422d5388e1dbd3365
saeid rouhparvar <saeid.rohparvar@gmail.com>
2025-02-06 13:03:33 +0330

    create circle chart

---
+++ src/components/circleChart.tsx
@@ -0,0 +1,96 @@
+import { Box, Flex, SimpleGrid } from "@chakra-ui/react";
+import ApexChart from "./Chart";
+import { lightArrow } from "./svg";
+
+const chartLabel = [
+  {
+    title: "بسته‌بندی",
+    color: "#E64A19",
+    percent: "(+2.98%)",
+    develope: "39.11%",
+    status: "success",
+  },
+  {
+    title: "فروش",
+    color: "#FF5722",
+    percent: "(-3.25%)",
+    develope: "28.02%",
+    status: "failed",
+  },
+  {
+    title: "بخش‌های تولید",
+    color: "#F57C00",
+    percent: "(+0.14%)",
+    develope: "23.13%",
+    status: "success",
+  },
+  {
+    title: "دیگر",
+    color: "#FFB74D",
+    percent: "(-1.11%)",
+    develope: "5.03%",
+    status: "failed",
+  },
+];
+
+const CircleChart = () => {
+  return (
+    <Flex
+      direction={"column"}
+      gap={4}
+      backgroundColor={"white"}
+      p={6}
+      borderRadius={"20px"}
+    >
+      <Flex align={"center"} justify={"space-between"}>
+        <Flex direction={"column"}>
+          <Box color={"#757575"}>درصد مشارکت بخش‌های مختلف</Box>
+          <Box color={"#212121"} fontSize={18}>
+            در فرآیند تولید یا فروش
+          </Box>
+        </Flex>
+
+        <Flex
+          color={"#615E83"}
+          bg={"#F8F8FF"}
+          borderRadius={"20px"}
+          px={4}
+          py={1}
+          gap={4}
+        >
+          <Box>1404</Box>
+          <Box mt={2}>{lightArrow}</Box>
+        </Flex>
+      </Flex>
+      <SimpleGrid columns={{ base: 1, md: 2 }} alignItems={"center"} gap={4}>
+        <ApexChart
+          categories={["Product A", "Product B", "Product C"]}
+          series={[30, 40, 50, 60]}
+          type="pie"
+          height="240px"
+        />
+        <Flex direction={"column"} gap={5}>
+          {chartLabel.map((i, index) => (
+            <Flex justify={"space-between"} align={"center"}>
+              <Flex align={"center"} gap={2} key={index}>
+                <Box
+                  width={"9px"}
+                  height={"9px"}
+                  backgroundColor={i.color}
+                  borderRadius={"100%"}
+                ></Box>
+                <Box>{i.title}</Box>
+              </Flex>
+              <Flex gap={4} fontSize={12}>
+                <Box color={i.status === 'success' ? "#04CE00" : '#FF718B'}>{i.percent}</Box>
+                <Box>{i.develope}</Box>
+              </Flex>
+            </Flex>
+          ))}
+        </Flex>
+      </SimpleGrid>
+    </Flex>
+  );
+};
+
+export default CircleChart;
