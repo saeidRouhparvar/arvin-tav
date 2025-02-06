d51794d2fa97de84fb80ba4c97640754902dd644
saeid rouhparvar <saeid.rohparvar@gmail.com>
2025-02-06 13:03:44 +0330

    create main chart

--- src/components/mainChart.tsx
+++ src/components/mainChart.tsx
@@ -1,8 +1,54 @@
+import { Flex } from "@chakra-ui/react";
+import ApexChart from "./Chart";
+import ChartTopBar from "./chartTopBar";
+import { useState } from "react";
 
 const MainChart = () => {
-  return (
-    <div>MainChart</div>
-  )
-}
+  const [series] = useState([
+    {
+      name: "کاهش هزینه ها",
+      data: [4, 6, 5, 6, 8, 5, 4, 6, 7],
+    },
+    {
+      name: "سرعت تولید",
+      data: [1, 3, 9, 4, 5, 5, 4, 2, 9],
+    },
+    {
+      name: "دانش های ثبت شده",
+      data: [6, 4, 6, 3, 2, 9, 6, 3, 5],
+    },
+  ]);
 
-export default MainChart
+  console.log(series);
+  
+  return (
+    <Flex
+      p={"2rem"}
+      direction={"column"}
+      gap={4}
+      backgroundColor={"white"}
+      borderRadius={"20px"}
+      w={"100%"}
+    >
+      <ChartTopBar />
+      <ApexChart
+      height="450"
+        type="line"
+        series={series}
+        categories={[
+          "1 فروردین",
+          "فروردین 6",
+          "فروردین 11",
+          "فروردین 17",
+          "24 فروردین",
+          "31 فروردین",
+          "5 اردیبهشت",
+          "12 اردیبهشت",
+          "19 اردیبهشت",
+        ]}
+      />
+    </Flex>
+  );
+};
+
+export default MainChart;
