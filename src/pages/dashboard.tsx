43c4cf927652bef28808f55bfaf24dc187b42862
saeid rouhparvar <saeid.rohparvar@gmail.com>
2025-02-06 13:05:48 +0330

    refactor dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,18 +1,18 @@
-import { Flex } from "@chakra-ui/react"
-import Topbar from "../components/topbar"
-import Statistics from "../components/statistics"
-import MainChart from "../components/mainChart"
-import OtherCharts from "../components/otherCharts"
+import { Flex } from "@chakra-ui/react";
+import Topbar from "../components/topbar";
+import Statistics from "../components/statistics";
+import MainChart from "../components/mainChart";
+import OtherCharts from "../components/otherCharts";
 
 const Dashboard = () => {
   return (
-   <Flex direction={'column'} gap={'1.5rem'}>
-    <Topbar/>
-    <Statistics/>
-    <MainChart/>
-    <OtherCharts/>
-   </Flex>
-  )
-}
+    <Flex direction={"column"} gap={"1.5rem"}>
+      <Topbar />
+      <Statistics />
+      <MainChart />
+      <OtherCharts />
+    </Flex>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
